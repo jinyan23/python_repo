3f51a60608b135f2eaa2e9dcc406d9c8eeb50e2c
laozhang22 <zhangjinyan@outlook.sg>
2023-01-28 16:28:16 +0800

    added bus route database retrieval.

--- bus_arrival/bus_database.py
+++ bus_arrival/bus_database.py
@@ -15,16 +15,13 @@ h = http.Http()
 headers = {"AccountKey": key[0],
            "accept": "application/json"}
 
-# Define function to retrieve all available bus stops in Singapore
-def retrieve_bus_stops():
-    
-    # Create list of containers
-    busStopCode = list()
-    roadName = list()
-    description = list()
-    latitude = list()
-    longitude = list()
 
+# Define function to retrieve all available bus stops in Singapore
+def bus_database():
+    
+    # Create empty list to hold list of bus stops
+    bsObj = list()
+    
     # Each API call only returns 500 records.
     # Total number of bus stops in Singapore slightly > 5,000
     for i in range(0, 5500, 500):
@@ -39,20 +36,43 @@ def retrieve_bus_stops():
             headers
             )
         
-        jsonObj = json.loads(content)
-        
-        busStopCode.extend([i["BusStopCode"] for i in jsonObj["value"]])
-        roadName.extend([i["RoadName"] for i in jsonObj["value"]])
-        description.extend([i["Description"] for i in jsonObj["value"]])
-        latitude.extend([i["Latitude"] for i in jsonObj["value"]])
-        longitude.extend([i["Longitude"] for i in jsonObj["value"]])
-
-    # Build all the responses into a dataframe for output
-    bs_df = pd.DataFrame({"busStopCode": busStopCode,
-                        "roadName": roadName,
-                        "description": description,
-                        "latitude": latitude,
-                        "longitude": longitude})
+        bsObj.extend(json.loads(content)["value"])
     
-    return bs_df
+    # Build all the responses into a dataframe for output    
+    bs_df = pd.DataFrame({"busStopCode": [i["BusStopCode"] for i in bsObj],
+                          "roadName": [i["RoadName"] for i in bsObj],
+                          "description": [i["Description"] for i in bsObj],
+                          "latitude": [i["Latitude"] for i in bsObj],
+                          "longitude": [i["Longitude"] for i in bsObj]})
 
+
+    # Retrieve list of all service routes in Singapore
+    # Contains arrival sequence of bus at bus stop
+
+    brObj = list()
+
+    for i in range(0, 27000, 500):
+        
+        target = urlparse(uri + "ltaodataservice/BusRoutes?$skip=" + str(i))
+        
+        response, content = h.request(
+            target.geturl(),
+            method,
+            body,
+            headers)
+
+        brObj.extend(json.loads(content)["value"])
+
+    br_df = pd.DataFrame({"serviceNo": [i["ServiceNo"] for i in brObj],
+                        "busStopCode": [i["BusStopCode"] for i in brObj],
+                        "busStopSeq": [i["StopSequence"] for i in brObj],
+                        "direction": [i["Direction"] for i in brObj]})
+
+    bus_db = br_df.merge(bs_df, on="busStopCode")
+    
+    
+    return bus_db
+
+
+if __name__ == "__main__":
+    bus_database()
