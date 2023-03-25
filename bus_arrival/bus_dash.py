ea550b6953bd10a488436d516ab07248b1f55192
laozhang22 <zhangjinyan@outlook.sg>
2023-03-25 14:51:49 +0800

    formatted app table format.

--- bus_arrival/bus_dash.py
+++ bus_arrival/bus_dash.py
@@ -11,9 +11,19 @@ from bus import bus, route, nearest
 
 warnings.filterwarnings('ignore')
 
-# Python dash app style sheets
+#-------------------------------#
+# Python dash app style sheets. #
+#-------------------------------#
+
+
 app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP],
            suppress_callback_exceptions=True)
+
+
+#------------------------------------------------------#
+# Setting the styles of the sidebar and content pages. #
+#------------------------------------------------------#
+
 
 sidebar_style = {
     "position": "fixed",
@@ -24,12 +34,12 @@ sidebar_style = {
     "padding": "2rem 1rem",
     "background-color": "#f8f9fa",
 }
-
 content_style = {
     "margin-left": "18rem",
     "margin-right": "2rem",
     "padding": "2rem 1rem",
 }
+
 
 sidebar = html.Div(
     [
@@ -48,18 +58,21 @@ sidebar = html.Div(
     ],
     style=sidebar_style,
 )
-
-
 content = html.Div(id="page-content", style=content_style)
 
 
-# Set the content of the pages here
+#--------------------------------------------#
+# Set the content of the content pages here. #
+#--------------------------------------------#
+
+
 phome_content = html.Div(children=[
     html.H3("Bus Arrival Application"),
     html.P("This is a web application that displays bus arrival timings, bus routes and locations of bus stops in Singapore. Data is retrieved via LTA Datamall API calls. This application displayed on the browser using Python Dash interactive interface.")
 ])
 pone_content = html.Div(children=[
     html.H4("Bus Arrival Timings"),
+    html.P("The timings are in minutes. (E.g. 4 = 4 minutes till bus arrival at queried bus stop.) If the bus is arriving within a minute, it'll show 'Arr'. If the cell is empty, it means the LTA Datamall doesn't the data for it. It might also mean the last service or certain gaps between two services that are not yet updated."),
     html.P("Enter Bus Stop Code", className="lead"),
     dcc.Input(id="my-input", value='04111', type='text'),
     html.Button("Submit", id="arr-button", n_clicks=0),
@@ -72,6 +85,7 @@ ptwo_content = html.Div(children=[
         html.P("In mobile application for nearest bus stops display, the geolocation of the individual can be obtained via the GPS service available on the phone. However, for this web application, that service is unavailable. Thus, the geolocation for this demonstration is hard coded in. For mobile app, the geolocation will be updated, and the data will be used to calculate the distance to the nearest bus stops, thus allowing the display of the list of bus stops within the near vicinity of the individual."),
         html.P("The bus stops displayed here are within 500 metres from this coordinates: [1.310429, 103.854368] along Serangoon Road near Farrer Park MRT Station."),
         dcc.Input(id="near-input", value="1.310429, 103.854368", type='text'),
+        html.Button("Submit", id="near-button", n_clicks=0),
         html.Hr()
     ]),
     html.Div(id="near-output")
@@ -87,11 +101,21 @@ pthree_content = html.Div(children=[
     ])])
 
 
-# app.layout
+#------------------------------------------#
+# Determine the layout of the application. #
+#------------------------------------------#
+
+
+# app was instantiated with Dash() above. 
 app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
 
 
-# app.callback for navigation panel
+#--------------------------------------------#
+# Interactive callbacks for the application. #
+#--------------------------------------------#
+
+
+# Display content page based on selection in sidebar.
 @app.callback(
     Output("page-content", "children"), 
     Input("url", "pathname")
@@ -116,6 +140,18 @@ def render_page_content(pathname):
     )
 
 
+table_style = dict(border="2px solid",
+                       width="80%",
+                       textAlign="center")
+    
+head_style = dict(fontSize="20px", 
+                  backgroundColor="#003000",
+                  color="white")
+    
+body_style = dict(border="1px solid",
+                  borderLeft="1px solid",
+                  borderRight="1px solid",)
+
 # app.callback for bus arrival timing (pane 1)
 @app.callback(
     Output(component_id="my-output", component_property="children"),
@@ -129,40 +165,48 @@ def update_table(n_clicks, input_value):
     return html.Table([
         
         html.Thead(
-            html.Tr([html.Th(col) for col in df.columns])
+            html.Tr([html.Th(col, style=head_style) for col in df.columns])
         ),
         
         html.Tbody([
             html.Tr([
-                html.Td(df.iloc[i][col]) for col in df.columns  # type: ignore
-            ]) for i in range(min(len(df), 30))
-        ])
-        
-    ])
+                html.Td(df.iloc[i][col], style=body_style) for col in df.columns  # type: ignore
+            ], style=body_style) for i in range(min(len(df), 30))
+        ]),
+           
+    ],
+    
+    style=table_style
+    
+    )
 
 # app.callback for nearest bus list (pane 2)
 @app.callback(
     Output(component_id="near-output", component_property="children"),
-    Input(component_id="near-input", component_property="value")
+    Input(component_id="near-button", component_property="n_clicks"),
+    State(component_id="near-input", component_property="value")
 )
-def update_bus_stops(input_value):
+def update_bus_stops(n_clicks, input_value):
     
-    bs_output = nearest()
-    
+    bs_output = nearest(input_value)
+        
     return html.Table([
         
         html.Thead(
-            html.Tr([html.Th(col) for col in bs_output.columns])
+            html.Tr([html.Th(col, style=head_style) for col in bs_output.columns])
         ),
         
         html.Tbody([
             html.Tr([
-                html.Td(bs_output.iloc[i][col]) for col in bs_output.columns  # type: ignore
+                html.Td(bs_output.iloc[i][col], style=body_style) for col in bs_output.columns  # type: ignore
             ]) for i in range(min(len(bs_output), 30))
         ])
         
-    ])
-
+    ],
+                      
+    style=table_style
+    
+    )
 
 # app.callback for bus route map (pane 3)
 @app.callback(
@@ -193,6 +237,11 @@ def update_map(n_clicks, input_value):
                     showlegend=False)
     
     return fig
-    
+
+
+#------#
+# End. #
+#------#
+
 if __name__ == "__main__":
     app.run_server(debug=True)
